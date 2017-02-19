fccb938a5b56825103db87c8a41ceda5cdc9b762
Eugénie <Eugnie@PC_EUGNIE>
2017-02-19 18:09:48 +0100

    test

--- NumberToChar/Program.cs
+++ NumberToChar/Program.cs
@@ -21,7 +21,7 @@ namespace NumberToChar
             string test12 = ToChar(340);
             string test13 = ToChar(931);
 
-            Console.WriteLine("Saisisez le nombre entre 0 et 999 à convertir en lettre");
+            Console.WriteLine("Saisisez le nombre entre 0 et 999 à convertir en lettre svp");
             string saisie = Console.ReadLine();
 
             int convert;
